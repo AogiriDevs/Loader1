a8f55c22cfd8719c079889c0b03208838f23a436
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-07-29 01:44:55 +0530

    Update __init__.py

--- loader/core/__init__.py
+++ loader/core/__init__.py
@@ -6,5 +6,5 @@ MAX_PY = 3.9
 CONF_PATH = "config.env"
 
 # will be removed after pluggable utils and res
-CORE_REPO = environ.get('CORE_REPO', "https://github.com/AogiriDevs/Loader1")
-CORE_BRANCH = environ.get('CORE_BRANCH', "master")
+CORE_REPO = environ.get('CORE_REPO', "https://github.com/UsergeTeam/Loader")
+CORE_BRANCH = environ.get('CORE_BRANCH', "beta")
